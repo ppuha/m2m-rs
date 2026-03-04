a115a16cc10c8b554061ef327cfe5e3b0d589061
Péter Puha <peterpuha@Peters-MacBook-Air-2.local>
2026-03-04 19:19:12 +0100

    use in memory store for clients

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,45 @@
+#[derive(Clone)]
 pub struct Client {
     pub client_id: String,
-    client_secret: String,
+    client_secret: Option<String>,
 }
 
-pub fn auth_client(client_id: String, client_secret: String) -> Client {
-    Client {
-        client_id: client_id,
-        client_secret: client_secret,
+async fn get_clients() -> Vec<Client> {
+    vec![
+        Client {
+            client_id: "client0".to_string(),
+            client_secret: None,
+        },
+        Client {
+            client_id: "client1".to_string(),
+            client_secret: Some("foobar".to_string()),
+        },
+        Client {
+            client_id: "client2".to_string(),
+            client_secret: Some("secret".to_string()),
+        },
+    ]
+}
+
+pub async fn auth_client(client_id: String, client_secret: String) -> Result<Client, String> {
+    let client = get_clients()
+        .await
+        .into_iter()
+        .find(|c| c.client_id == client_id);
+
+    match client {
+        Some(c) => {
+            if c.client_secret
+                .to_owned()
+                .map(|cs| cs == client_secret)
+                .or_else(|| Some(true))
+                .unwrap()
+            {
+                Ok(c.clone())
+            } else {
+                Err("invalid credentials".to_string())
+            }
+        }
+        None => Err("unknown client".to_string()),
     }
 }
--- src/handler.rs
+++ src/handler.rs
@@ -1,5 +1,6 @@
-use actix_web::web;
-use serde::Deserialize;
+use actix_web::web::Json;
+use actix_web::{HttpResponse, web};
+use serde::{Deserialize, Serialize};
 
 #[path = "./client.rs"]
 mod client;
@@ -11,13 +12,28 @@ mod token;
 pub struct TokenForm {
     client_id: String,
     client_secret: String,
-    _grant_type: String,
+    grant_type: String,
 }
 
-pub async fn get_token(form: web::Form<TokenForm>) -> web::Json<token::Token> {
-    let form_data = form.into_inner();
-    let client = client::auth_client(form_data.client_id, form_data.client_secret);
-    let token = token::generate_token(client.client_id);
+#[derive(Serialize)]
+pub struct TokenError {
+    error: String,
+}
 
-    web::Json(token)
+pub async fn get_token(form: web::Form<TokenForm>) -> HttpResponse {
+    let form_data = form.into_inner();
+    if form_data.grant_type != "client_credentials" {
+        HttpResponse::BadRequest().json(Json(TokenError {
+            error: "invalid grant type".to_string(),
+        }))
+    } else {
+        let auth_result = client::auth_client(form_data.client_id, form_data.client_secret).await;
+        match auth_result {
+            Ok(client) => {
+                let token = token::generate_token(client.client_id);
+                HttpResponse::Ok().json(Json(token))
+            }
+            Err(err) => HttpResponse::BadRequest().json(TokenError { error: err }),
+        }
+    }
 }
